9764fd6727422bada4a89cb0ae1b84e81b143ac0
Alexa13422 <andr.alex04@gmail.com>
2024-10-28 13:39:31 +0100

    initial

---
+++ src/main/java/org/example/lastcommoncommits_test/LastCommonCommitsTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.lastcommoncommits_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastCommonCommitsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastCommonCommitsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lastcommoncommits_test/LastCommonCommitsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lastcommoncommits_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastCommonCommitsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
